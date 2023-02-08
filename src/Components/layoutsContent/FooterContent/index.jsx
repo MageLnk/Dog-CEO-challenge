794286dad1da38cf26fe9e9d29fcc4fe7da27f1a
MageLink <mage.link.zhezard@gmail.com>
2023-02-08 04:21:05 -0300

    React-icon added and footer content ok

--- src/Components/layoutsContent/FooterContent/index.jsx
+++ src/Components/layoutsContent/FooterContent/index.jsx
@@ -1,8 +1,17 @@
 // Style
 import "./style.css";
+import { BiRegistered } from "react-icons/bi";
 // App
 const FooterContent = () => {
-  return <div className="footer-content-container">Footer o k ase</div>;
+  return (
+    <div className="footer-content-container">
+      <div className="footer-content-container">
+        <span>
+          Página creada por poderoso MageLink como prueba técnica utilizando la API de Dog CEO. <BiRegistered />
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default FooterContent;
